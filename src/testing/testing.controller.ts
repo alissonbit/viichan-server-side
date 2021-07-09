be1ebe7fe58c6a0acd50f7f9e6dcf2ffab0b2a31
alisson <alissongilson.bezerra@gmail.com>
2021-07-09 20:45:49 -0300

    feat: testing controller

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('testing')
 export class TestingController {
   @Get()
-  testConnection(){
-    return 'testing'
+  test(): any{
+    return true;
   }
 }
